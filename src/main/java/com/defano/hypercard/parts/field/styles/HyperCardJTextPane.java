2eeca050a291d355868cf4b1aca354904d2a484f
Matt DeFano <matt@defano.com>
2017-08-16 20:45:41 -0500

    Substantial improvement to field rendering speed
    Affects fields with visible lines.

--- src/main/java/com/defano/hypercard/parts/field/styles/HyperCardJTextPane.java
+++ src/main/java/com/defano/hypercard/parts/field/styles/HyperCardJTextPane.java
@@ -2,10 +2,8 @@ package com.defano.hypercard.parts.field.styles;
 
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
-import javax.swing.text.Utilities;
+import javax.swing.text.View;
 import java.awt.*;
 
 /**
@@ -57,11 +55,11 @@ public class HyperCardJTextPane extends JTextPane {
 
     /** {@inheritDoc} */
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
         if (showLines) {
-            int lastLineHeight = getHeightForAttributes(g, getCharacterAttributes());
+            float lastLineHeight = 0;
             int dottedLineY = 0;
             int lineCount = getWrappedLineCount();
 
@@ -70,7 +68,7 @@ public class HyperCardJTextPane extends JTextPane {
 
             // Draw dotted line under all lines with text
             for (int line = 0; line < lineCount; line++) {
-                lastLineHeight = getLineHeight(g, line);
+                lastLineHeight = getLineHeight(line);
                 dottedLineY += lastLineHeight;
                 g.drawLine(0, dottedLineY, getWidth(), dottedLineY);
             }
@@ -83,88 +81,31 @@ public class HyperCardJTextPane extends JTextPane {
         }
     }
 
-    private int getLineHeight(Graphics g, int line) {
-        int maxHeight = 0;
+    /**
+     * Determines the rendered height of the request line index.
+     *
+     * @param line The index of the line whose height should be calculated; a value between 0 and {@link #getWrappedLineCount()}
+     * @return The height, in pixels.
+     */
+    private float getLineHeight(int line) {
+        int lineCount = 0;
+        View document = getUI().getRootView(this).getView(0);
 
-        if (line > getWrappedLineCount()) {
-            return getHeightForAttributes(g, getCharacterAttributes());
-        }
+        // Walk each paragraph in document
+        for (int paragraphIdx = 0; paragraphIdx < document.getViewCount(); paragraphIdx++) {
+            View paragraph = document.getView(paragraphIdx);
 
-        for (int index = getLineStartOffset(line); index <= getLineEndOffset(line); index++) {
-            int height = getHeightForAttributes(g, getStyledDocument().getCharacterElement(index).getAttributes());
+            // Walk each line in paragraph
+            for (int lineIdx = 0; lineIdx < paragraph.getViewCount(); lineIdx++) {
 
-            if (height > maxHeight) {
-                maxHeight = height;
+                // Did we find requested line?
+                if (lineCount++ == line) {
+                    return paragraph.getView(lineIdx).getPreferredSpan(View.Y_AXIS);
+                }
             }
         }
 
-        return maxHeight;
-    }
-
-    private int getHeightForAttributes(Graphics g, AttributeSet attributes) {
-        return g.getFontMetrics(getStyledDocument().getFont(attributes)).getHeight();
-    }
-
-    private int getLineEndOffset(int line) {
-
-        // Special case: Empty field
-        if (getDocument().getLength() == 0) {
-            return 0;
-        }
-
-        int thisLine = 0;
-        int lastRowStart = 0;
-
-        for (int index = 0; index < getDocument().getLength(); index++) {
-            try {
-                int thisRowStart = Utilities.getRowStart(this, index);
-
-                if (lastRowStart != thisRowStart) {
-                    lastRowStart = thisRowStart;
-                    thisLine++;
-                }
-
-                if (thisLine == line) {
-                    return Utilities.getRowEnd(this, index);
-                }
-
-            } catch (BadLocationException e) {
-                throw new IllegalStateException("Bug! Caught exception measuring text.", e);
-            }
-        }
-
-        // No such line number
-        return -1;
-    }
-
-    private int getLineStartOffset(int line) {
-
-        // Special case: Line 0 always starts at char 0.
-        if (line == 0) {
-            return 0;
-        }
-
-        int thisLine = 0;
-        int lastRowStart = 0;
-
-        for (int index = 0; index < getDocument().getLength(); index++) {
-            try {
-                int thisRowStart = Utilities.getRowStart(this, index);
-                if (lastRowStart != thisRowStart) {
-                    lastRowStart = thisRowStart;
-                    thisLine++;
-
-                    if (thisLine == line) {
-                        return index;
-                    }
-                }
-            } catch (BadLocationException e) {
-                throw new IllegalStateException("Bug! Caught exception measuring text.", e);
-            }
-        }
-
-        // No such line number
-        return -1;
+        return 0;
     }
 
     /**
@@ -173,29 +114,19 @@ public class HyperCardJTextPane extends JTextPane {
      *
      * @return The number of lines of text in the field; some lines may be wrapped.
      */
-    private int getWrappedLineCount() {
+    private int getWrappedLineCount()
+    {
+        int lines = 0;
+        View document = getUI().getRootView(this).getView(0);
 
-        // Special case, empty document has zero lines
-        if (getDocument().getLength() == 0) {
-            return 0;
+        int paragraphs = document.getViewCount();
+
+        for (int i = 0; i < paragraphs; i++) {
+            View paragraph = document.getView(i);
+            lines += paragraph.getViewCount();
         }
 
-        int thisLine = 1;
-        int lastRowStart = 0;
-
-        for (int index = 0; index < getDocument().getLength(); index++) {
-            try {
-                int thisRowStart = Utilities.getRowStart(this, index);
-                if (lastRowStart != thisRowStart) {
-                    lastRowStart = thisRowStart;
-                    thisLine++;
-                }
-            } catch (BadLocationException e) {
-                throw new IllegalStateException("Bug! Caught exception measuring text.", e);
-            }
-        }
-
-        return thisLine;
+        return lines;
     }
 
 }
