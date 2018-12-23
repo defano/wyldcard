c2e0dcb71bfac6084a7c365642ee1ea6f2421c4a
Matt DeFano <matt@defano.com>
2018-12-23 10:44:28 -0600

    Added AddCmd and AverageFunc tests

---
+++ src/test/java/com/defano/hypertalk/ast/expressions/functions/AverageFuncTest.java
@@ -0,0 +1,68 @@
+package com.defano.hypertalk.ast.expressions.functions;
+
+import com.defano.hypertalk.GuiceTest;
+import com.defano.hypertalk.ast.expressions.Expression;
+import com.defano.hypertalk.ast.model.Value;
+import com.defano.hypertalk.exception.HtSemanticException;
+import com.defano.wyldcard.runtime.context.ExecutionContext;
+import com.google.common.collect.Lists;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class AverageFuncTest extends GuiceTest<AverageFunc> {
+
+    private Expression mockExpression = Mockito.mock(Expression.class);
+
+    @BeforeEach
+    public void setUp() {
+        initialize(new AverageFunc(mockParserRuleContext, mockExpression));
+    }
+
+    @Test
+    public void testInvalidArguments() throws Exception {
+        // Setup
+        Mockito.when(mockExpression.evaluateAsList(mockExecutionContext)).thenReturn(Lists.newArrayList(
+                new Value("barf"), new Value(3), new Value(5), new Value(7)
+        ));
+
+        // Run the test
+        assertThrows(HtSemanticException.class, () -> uut.onEvaluate(mockExecutionContext));
+    }
+
+    @Test
+    public void testNoArguments() throws Exception {
+        // Setup
+        final Value expectedResult = new Value(0);
+
+        Mockito.when(mockExpression.evaluateAsList(mockExecutionContext)).thenReturn(Lists.newArrayList());
+
+        // Run the test
+        final Value result = uut.onEvaluate(mockExecutionContext);
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testAverageCalculation() throws Exception {
+        // Setup
+        final Value expectedResult = new Value(4.0);
+
+        Mockito.when(mockExpression.evaluateAsList(mockExecutionContext)).thenReturn(Lists.newArrayList(
+                new Value(1), new Value(3), new Value(5), new Value(7)
+        ));
+
+        // Run the test
+        final Value result = uut.onEvaluate(mockExecutionContext);
+
+        // Verify the results
+        assertEquals(expectedResult, result);
+    }
+}
